21731d358f19ba8f19de5bd971682ac2be3f06ca
Coco <cocochen12301@gmail.com>
2025-04-30 01:11:46 -0700

    docs: Updated react page content

--- src/info-content/AboutPage.tsx
+++ src/info-content/AboutPage.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 // Icons
+import link from "../assets/images/icons/external-link.png";
 import ts from "../assets/images/icons/typescript.png";
 import js from "../assets/images/icons/javascript.png";
 import threejs from "../assets/images/icons/threejs.png";
@@ -11,7 +12,9 @@ const AboutPage: React.FC = () => {
   return (
     <div className="information__body" id="info-body">
       <div className="title__container">
-        <h2 className="title">ABOUT THIS WEBSITE</h2>
+        <h2 className="title">ABOUT THIS WEBSITE&nbsp;&nbsp;
+          <a href="https://github.com/cocochen01/cocochen01.github.io" target="_blank"><img src={link} className="external-link" style={{ width: "3%"}}></img></a>
+        </h2>
         <div className="icon__container">
           <div className="icons">
             <img src={threejs} alt="ThreeJS" style={{float: "right", width: "18%", transform: "translate(-50%, -50%)", margin: "2%"}}></img>
@@ -25,11 +28,9 @@ const AboutPage: React.FC = () => {
         </div>
       </div>
       <p>
-          This website was built from scratch with Node.js and Vite, and uses Three.js to render a 3D model.<br />
-          The website layout is to emulate the appearance of an Action RPG invectory. Three.js is a Javascript 3D library that is often used to make browser games.
-          For this website, it renders a 3D model I made and allows the user to rotate it, just like you would in your RPG inventory.<br />
-          Much care was taken to ensure everything, model included, scales with the window size, feel free to test it out!<br />
-          I used the official documentation for Three.js as my guide for implementing the model, their website is at <a href="https://threejs.org">threejs.org</a>.
+          This website was first built from scratch with React + Vite in 2022, it has since been refactored and improved for code legibility.<br /><br />
+          Three.js and React-Three-Fiber are used to render the 3D model, Drei is a helper library I used for OrbitControls which lets the user rotate the model.
+          A custom react hook scales the font sizes for different viewport sizes. You can observe that when the window is resized, all text on the page scales along.<br />
       </p>
     </div>
   );
