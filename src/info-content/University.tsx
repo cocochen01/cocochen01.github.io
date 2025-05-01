6b7bda100b41ae41cce97332b748cb814fba12bd
Coco <cocochen12301@gmail.com>
2025-05-01 01:00:58 -0700

    docs: Updated University content

--- src/info-content/University.tsx
+++ src/info-content/University.tsx
@@ -6,18 +6,18 @@ const University: React.FC = () => {
       <h2>UNIVERSITY OF CALIFORNIA, RIVERSIDE&nbsp;&nbsp;<span style={{ textShadow: "none", fontSize: "0.7em", color: "#ffffff80" }}>2019 - 2023</span></h2>
       <p><b>B.S. in Computer Science</b></p>
       <p>
-        While attending UCR, I participated in various Hackathons and programming competitions.
+        While attending UCR, participated in various Hackathons and programming competitions.
       </p>
       <p></p>
       <p></p>
       <p><b>NOTABLE COURSES</b></p>
       <p>
-        Computer Graphics<br />
         Principles of Web Development<br />
+        Computer Graphics<br />
+        Computer Security<br />
         Database Management Systems<br />
-        Design of Operating Systems<br />
         Information Retrieval<br />
-        Computer Security
+        Design of Operating Systems
       </p>
     </div>
   );
